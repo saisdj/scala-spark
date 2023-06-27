35850e6087b96e8c0d2efd22a7245982b02d4078
Sachin Jha <118155820+saisdj@users.noreply.github.com>
2023-06-27 18:55:04 +0530

    Update PrintSeries_5_1.scala

--- src/main/scala/PrintSeries_5_1.scala
+++ src/main/scala/PrintSeries_5_1.scala
@@ -1,10 +1,7 @@
 object PrintSeries_5_1 {
   def main(args: Array[String]): Unit = {
 
-    val a = 5
-    val b = 11
-
-    for (i <- a to -b by -1) {
+    for (i <- 5 to -11 by -1) {
       val c = i - 1
       println(s"$a*$c", a * c)
     }
